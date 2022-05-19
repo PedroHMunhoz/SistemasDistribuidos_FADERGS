0197ca23d40aeff2d7f9d056d014f11fe8aaef3a
Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>
2022-05-19 17:45:57 -0300

    Implementada assinatura dos métodos na interface
    Signed-off-by: Rodrigo Alves Fontanella <fontanellarodrigo13@gmail.com>

--- TrabalhoPratico_RMI/InterfaceRMI.java
+++ TrabalhoPratico_RMI/InterfaceRMI.java
@@ -2,4 +2,61 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface InterfaceRMI extends Remote {
+    //Interface:
+
+public int getFatorial (int fatorial) throws RemoteException;
+public String dataAtual () throws RemoteException;
+public String horaAtual () throws RemoteException;
+public String boasVindas(String nomeUsuario) throws RemoteException;
+public int ordenarLista(int[] listaOrdenada)  throws RemoteException; 
+public boolean validarCPF(String cpf)    throws RemoteException;       
+
+
+int result = 0;
+
+public int getFatorial (int fatorial) {
+	
+	while(fatorial < 1) {
+		
+		result+=fatorial*(fatorial-1);
+		fatorial--;
+	}
+}
+//
+public String dataAtual () {
+	
+	SimpleDateFormat form = new SimpleDateFormat("dd/mm/yyyy");
+	String data = new Date().toString();
+
+return form.format(data);
+
+}
+//
+public String horaAtual () {
+	
+	SimpleDateFormat form = new SimpleDateFormat("hh:mm:ss");
+	String hora = new Date().toString();
+
+return form.format(hora);
+	
+}
+public String boasVindas (String nomeUsuario) {
+	
+	// System.out.println(objeto.boasVindas(nomeDigitado));
+}
+
+public int ordenarLista(int[] listaOrdenada)    {
+
+return int[]listaOrdenada;
+}
+
+
+public boolean validarCPF(String cpf)  {
+
+
+return boolean; 
+
+}
+
+                         
 }
